d45387a458a632805f616a90cc37d01227c908eb
jhontea <hafizhipb49@gmail.com>
2019-03-20 16:27:00 +0700

    add gin gonic

--- main.go
+++ main.go
@@ -1,5 +1,30 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
 
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	// load .env
+	fmt.Println("loading .env")
+	godotenv.Load()
+	fmt.Println("success loading .env")
+
+	route := gin.New()
+	route.Use(gin.Logger())
+	route.Use(gin.Recovery())
+
+	serverPort := os.Getenv("PORT")
+	serverString := fmt.Sprintf("run on port %s", serverPort)
+	fmt.Println(serverString)
+
+	if err := http.ListenAndServe(":"+serverPort, route); err != nil {
+		log.Fatal(err)
+	}
 }
